57305b1637996159cf13c9f0f1033735610b4cdf
fanux <fhtjob@hotmail.com>
2019-07-23 19:11:21 +0800

    add

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -it -v /Users/fanux/work/src/github.com/sealyun/saver:/etc/ansible fanux/sealos:v1.13.2 bash
+docker run -w /etc/ansible -it -v /Users/fanux/work/src/github.com/sealyun/saver:/etc/ansible fanux/sealos:v1.13.2 bash
